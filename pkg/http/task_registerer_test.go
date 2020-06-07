d31a55c07f2178149c78499d7fd3fcee7f6a4a01
Kowalski <artur.kowalski@nordcloud.com>
2020-06-07 20:37:45 +0200

    add task registerer tests

---
+++ pkg/http/task_registerer_test.go
@@ -0,0 +1,134 @@
+package http_test
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+	"time"
+
+	internalHTTP "github.com/nordcloud/termination-detector/pkg/http"
+	"github.com/nordcloud/termination-detector/pkg/task"
+	"github.com/stretchr/testify/assert"
+)
+
+type taskRegistererWithMocks struct {
+	requestExecutor *requestExecutorMock
+	taskRegisterer  *internalHTTP.TaskRegisterer
+}
+
+func newTaskRegistererWithMocks() *taskRegistererWithMocks {
+	requestExecutor := new(requestExecutorMock)
+	taskRegisterer := internalHTTP.NewTaskRegisterer(requestExecutor)
+	return &taskRegistererWithMocks{
+		requestExecutor: requestExecutor,
+		taskRegisterer:  taskRegisterer,
+	}
+}
+
+func TestTaskRegisterer_Register(t *testing.T) {
+	taskRegistererAndMocks := newTaskRegistererWithMocks()
+	taskExpirationTime := time.Now().Add(time.Hour)
+	taskToRegister := internalHTTP.Task{ExpirationTime: taskExpirationTime}
+	taskRegistrationData := task.RegistrationData{
+		ID: task.ID{
+			ProcessID: "1",
+			TaskID:    "2",
+		},
+		ExpirationTime: taskExpirationTime,
+	}
+	taskRegistererAndMocks.requestExecutor.On("ExecuteRequest", internalHTTP.Request{
+		Method:       internalHTTP.MethodPut,
+		ResourcePath: internalHTTP.ResourcePathTask,
+		Body:         taskToRegister.JSON(),
+		PathParameters: map[internalHTTP.PathParameter]string{
+			internalHTTP.PathParameterProcessID: taskRegistrationData.ID.ProcessID,
+			internalHTTP.PathParameterTaskID:    taskRegistrationData.ID.TaskID,
+		},
+	}).Return(internalHTTP.Response{
+		StatusCode: http.StatusCreated,
+	}, nil)
+
+	registrationStatus, err := taskRegistererAndMocks.taskRegisterer.Register(taskRegistrationData)
+	assert.NoError(t, err)
+	assert.Equal(t, task.RegistrationResultCreated, registrationStatus)
+}
+
+func TestTaskRegisterer_Register_TaskAlreadyRegistered(t *testing.T) {
+	taskRegistererAndMocks := newTaskRegistererWithMocks()
+	taskExpirationTime := time.Now().Add(time.Hour)
+	taskToRegister := internalHTTP.Task{ExpirationTime: taskExpirationTime}
+	taskRegistrationData := task.RegistrationData{
+		ID: task.ID{
+			ProcessID: "1",
+			TaskID:    "2",
+		},
+		ExpirationTime: taskExpirationTime,
+	}
+	taskRegistererAndMocks.requestExecutor.On("ExecuteRequest", internalHTTP.Request{
+		Method:       internalHTTP.MethodPut,
+		ResourcePath: internalHTTP.ResourcePathTask,
+		Body:         taskToRegister.JSON(),
+		PathParameters: map[internalHTTP.PathParameter]string{
+			internalHTTP.PathParameterProcessID: taskRegistrationData.ID.ProcessID,
+			internalHTTP.PathParameterTaskID:    taskRegistrationData.ID.TaskID,
+		},
+	}).Return(internalHTTP.Response{
+		StatusCode: http.StatusConflict,
+	}, nil)
+
+	registrationStatus, err := taskRegistererAndMocks.taskRegisterer.Register(taskRegistrationData)
+	assert.NoError(t, err)
+	assert.Equal(t, task.RegistrationResultAlreadyRegistered, registrationStatus)
+}
+
+func TestTaskRegisterer_Register_UnexpectedResponseStatus(t *testing.T) {
+	taskRegistererAndMocks := newTaskRegistererWithMocks()
+	taskExpirationTime := time.Now().Add(time.Hour)
+	taskToRegister := internalHTTP.Task{ExpirationTime: taskExpirationTime}
+	taskRegistrationData := task.RegistrationData{
+		ID: task.ID{
+			ProcessID: "1",
+			TaskID:    "2",
+		},
+		ExpirationTime: taskExpirationTime,
+	}
+	taskRegistererAndMocks.requestExecutor.On("ExecuteRequest", internalHTTP.Request{
+		Method:       internalHTTP.MethodPut,
+		ResourcePath: internalHTTP.ResourcePathTask,
+		Body:         taskToRegister.JSON(),
+		PathParameters: map[internalHTTP.PathParameter]string{
+			internalHTTP.PathParameterProcessID: taskRegistrationData.ID.ProcessID,
+			internalHTTP.PathParameterTaskID:    taskRegistrationData.ID.TaskID,
+		},
+	}).Return(internalHTTP.Response{
+		StatusCode: http.StatusInternalServerError,
+	}, nil)
+
+	_, err := taskRegistererAndMocks.taskRegisterer.Register(taskRegistrationData)
+	assert.Error(t, err)
+}
+
+func TestTaskRegisterer_Register_ExecutorError(t *testing.T) {
+	taskRegistererAndMocks := newTaskRegistererWithMocks()
+	taskExpirationTime := time.Now().Add(time.Hour)
+	taskToRegister := internalHTTP.Task{ExpirationTime: taskExpirationTime}
+	taskRegistrationData := task.RegistrationData{
+		ID: task.ID{
+			ProcessID: "1",
+			TaskID:    "2",
+		},
+		ExpirationTime: taskExpirationTime,
+	}
+	taskRegistererAndMocks.requestExecutor.On("ExecuteRequest", internalHTTP.Request{
+		Method:       internalHTTP.MethodPut,
+		ResourcePath: internalHTTP.ResourcePathTask,
+		Body:         taskToRegister.JSON(),
+		PathParameters: map[internalHTTP.PathParameter]string{
+			internalHTTP.PathParameterProcessID: taskRegistrationData.ID.ProcessID,
+			internalHTTP.PathParameterTaskID:    taskRegistrationData.ID.TaskID,
+		},
+	}).Return(internalHTTP.Response{}, errors.New("error"))
+
+	_, err := taskRegistererAndMocks.taskRegisterer.Register(taskRegistrationData)
+	assert.Error(t, err)
+}
